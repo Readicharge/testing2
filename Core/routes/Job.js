9177bd6d7010a131fcd830688a0db7cf5b421750
yashkumarsingh995 <yashyuvraj9020@gmail.com>
2023-10-02 16:02:26 +0530

    idk

---
+++ Core/routes/Job.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+
+// Import of the Job Controller
+const Job = require("../controllers/Job");
+
+
+// Creating the Job
+router.post("/",Job.createJob);
+
+// Getting all the Job
+router.get("/",Job.getJob);
+
+// Getting the Specific Job
+router.get("/:id",Job.getSpecificJob);
+    
+// Updating the Job 
+router.put("/:id",Job.updateJob);
+
+// Deleting the Job 
+router.delete("/:id",Job.deleteJob);
+
+
+module.exports = router;
